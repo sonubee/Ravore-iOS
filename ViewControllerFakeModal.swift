978e6699fac188d67da454395e10d9ffd3aab3e2
sonubee <gsbllc2011@gmail.com>
2016-04-22 14:37:23 -0700

    Before Login

--- ViewControllerFakeModal.swift
+++ ViewControllerFakeModal.swift
@@ -12,9 +12,6 @@ class ViewControllerFakeModal: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
-
         // Do any additional setup after loading the view.
     }
     
